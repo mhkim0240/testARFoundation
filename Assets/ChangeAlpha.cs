de7c0ab279552f41ccfa8c90314e66f4032f86f3
mhkim <dev.mhkim0240@gmail.com>
2025-05-19 16:55:17 +0900

    RuntimeInspector 추가

---
+++ Assets/ChangeAlpha.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAlpha : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        Invoke("cc", 1.0f);
+    }
+    void cc()
+    {
+var img = GetComponent<Image>();
+        img.color = new Color(img.color.r, img.color.g, img.color.b, 0.5f);
+    }
+
+}
