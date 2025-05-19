e32cf06b6b39e08100c58cdd81bacf38d30b7c1e
mhkim <dev.mhkim0240@gmail.com>
2025-05-19 17:35:37 +0900

    Reticle 오브젝트 추가

--- Assets/Starter Package/ReticleBehaviour.cs
+++ Assets/Starter Package/ReticleBehaviour.cs
@@ -30,6 +30,8 @@ public class ReticleBehaviour : MonoBehaviour
 
     public ARPlane CurrentPlane;
 
+    public Camera cam;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -39,5 +41,32 @@ public class ReticleBehaviour : MonoBehaviour
     private void Update()
     {
         // TODO: Conduct a ray cast to position this object.
+        var screenCenter = cam.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        DrivingSurfaceManager.RaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinBounds);
+
+        CurrentPlane = null;
+        ARRaycastHit? hit = null;
+
+
+        if (hits.Count > 0)
+        {
+            // If you don't have a locked plane already...
+            var lockedPlane = DrivingSurfaceManager.LockedPlane;
+            hit = lockedPlane == null
+                // ... use the first hit in `hits`.
+                ? hits[0]
+                // Otherwise use the locked plane, if it's there.
+                : hits.SingleOrDefault(x => x.trackableId == lockedPlane.trackableId);
+                
+                if (hit.HasValue)
+                {
+                    CurrentPlane = DrivingSurfaceManager.PlaneManager.GetPlane(hit.Value.trackableId);
+                    // Move this reticle to the location of the hit.
+                    transform.position = hit.Value.pose.position;
+                }
+                Child.SetActive(CurrentPlane != null);
+        }
+
     }
 }
